86170faca349241eaff2b277d968d60b85b7970a
Jacob <jacowest@gmail.com>
2022-06-17 22:57:30 +1000

    First shot

--- code/gen_ad.ps1
+++ code/gen_ad.ps1
@@ -4,7 +4,7 @@ function CreateADGroup () {
     param( [Parameter(Mandatory=$true)] $groupObject )
 
     $name - $groupObject.name
-    New-ADGroup -name $name -GroupScope Global
+    New-ADGroup -name $group_name -GroupScope Global
 }
 function CreateADUser() {
     param( [Parameter(Mandatory=$true)] $userObject )
@@ -24,8 +24,17 @@ function CreateADUser() {
     # Boom! create account
     New-ADUser -name "$name" -givenname $firstname -surname $lastname -SamAccoutName $samAccountName -userPrincipalName $principalname@$Global:Domain -accoutpasssword (ConvertTo-SecureString $password -AsPlainText -Force) -passthru | Enable-account
 
+    #add user to group
     foreach($group in $userObject.groups) {
-        Add-ADGroupMember -Identity $group -Members $username
+        try {
+            get-ADGroup -Identity "$group_name"
+            Add-ADGroupMember -Identity $group_name -Members $username
+        }
+        catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]
+        {
+            Write-Output "No AD Group by that name!"
+        }
+        
     }
     #Write-Output $userObject
 }
