b47bdb2721e6fd56eb200afe27d0fe4937179c76
Jacob <jacowest@gmail.com>
2022-06-17 23:39:45 +1000

    Script now works

--- code/gen_ad.ps1
+++ code/gen_ad.ps1
@@ -3,7 +3,7 @@ param([Parameter(Mandatory=$true)] $JSONFile)
 function CreateADGroup () {
     param( [Parameter(Mandatory=$true)] $groupObject )
 
-    $name - $groupObject.name
+    $group_name = $groupObject.name
     New-ADGroup -name $group_name -GroupScope Global
 }
 function CreateADUser() {
@@ -12,20 +12,23 @@ function CreateADUser() {
     #Pull info from JSON
     $name = $userObject.name
     $password = $userObject.password
-    $domain = 319b.local
+    $domain = "319b.local"
+    #$group_name = $userObject.groups
     
     
     #create FirstInitial lastname
     $firstname, $lastname = $name.Split(" ")
-    $username = ($firstname[0] + $lastname.tolower())
+    $username = ($firstname[0] + $lastname).tolower()
     $samAccountName = $username
     $principalname = $username
 
     # Boom! create account
-    New-ADUser -name "$name" -givenname $firstname -surname $lastname -SamAccoutName $samAccountName -userPrincipalName $principalname@$Global:Domain -accoutpasssword (ConvertTo-SecureString $password -AsPlainText -Force) -passthru | Enable-account
+    New-ADUser -name "$name" -givenname $firstname -surname $lastname -SamAccountName $samAccountName -userPrincipalName $principalname@$Global:Domain -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -passthru | Enable-ADAccount
+
+    Write-Output "User $name Created"
 
     #add user to group
-    foreach($group in $userObject.groups) {
+    foreach($group_name in $userObject.groups) {
         try {
             get-ADGroup -Identity "$group_name"
             Add-ADGroupMember -Identity $group_name -Members $username
@@ -44,7 +47,7 @@ $json  = (Get-Content $JSONFile | ConvertFrom-Json )
 $Global:Domain = $json.domain
 
 foreach ($group in $json.groups) {
-    CreateADUser $group
+    CreateADGroup $group
 }
 
 foreach ($user in $json.users) {
