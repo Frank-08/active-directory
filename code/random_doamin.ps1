64249fcac8a604f3985b9cc358c9661f54ee5549
Jacob <jacowest@gmail.com>
2022-08-01 22:35:11 +1000

    Add localadmin count

--- code/random_doamin.ps1
+++ code/random_doamin.ps1
@@ -1,4 +1,9 @@
-param([Parameter(Mandatory=$true)] $JSONOut)
+param(
+    [Parameter(Mandatory=$true)] $JSONFile,
+[int]$UserCount,
+[int]$GroupCount,
+[int]$LocalAdminCount
+)
 
 $group_names = [System.Collections.ArrayList](Get-Content "data/group_names.txt")
 $first_names = [System.Collections.ArrayList](Get-Content "data/first_names.txt")
@@ -7,9 +12,30 @@ $passwords = [System.Collections.ArrayList](Get-Content "data/passwords.txt")
 
 $groups = @()
 $users = @()
+
+
+#IF not stated when called default is here
+if ($UserCount -eq 0 ){
+    $UserCount = 5
+}
+
+if ($GroupCount -eq 0 ){
+    $GroupCount = 1
+}
+
+if ($LocalAdminCount -ne 0){
+    $local_admin_indexes = @()
+    while(($local_admin_indexes | Measure-Object ).Count -lt $LocalAdminCount){
+        $random_index = (Get-Random -InputObject (0..($UserCount-1)) | Where-Object { $local_admin_indexes -notcontains $_ })
+        $local_admin_indexes += @( $random_index )
+    }
+}
+
+echo $local_admin_indexes
+exit
+
 #GROUPS
-    $num_groups = 10
-    for ( $i =0; $i -lt $num_groups; $i++ ){
+    for ( $i =0; $i -lt $GroupCount; $i++ ){
     $new_group = (Get-Random -InputObject $group_names)
     $group = @{ "name" = "$new_group"}
     $groups += $group
@@ -17,8 +43,8 @@ $users = @()
     }
 
 #Users
-    $num_users = 100
-    for ( $i =0; $i -lt $num_users; $i++ ){
+
+    for ( $i =0; $i -lt $UserCount; $i++ ){
     $first_name = (Get-Random -InputObject $first_names)
     $last_name = (Get-Random -InputObject $last_names)
     $password = (Get-Random -InputObject $passwords)
